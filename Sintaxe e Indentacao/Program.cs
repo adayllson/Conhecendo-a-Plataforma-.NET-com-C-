9d3edab916670d57654505edd90177600a675e1d
Adailson <adayllson.almeida@gmail.com>
2022-09-19 17:28:54 -0300

    Sintaxe e Indentação

--- Sintaxe e Indentacao/Program.cs
+++ Sintaxe e Indentacao/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Sintaxe_e_Indentação.Models;
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Adailson";
+pessoa1.Idade = 31;
+pessoa1.Apresentar();
