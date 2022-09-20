d063dd12f959fb877025d21ed548cf98d06b6b16
Adailson <adayllson.almeida@gmail.com>
2022-09-20 16:29:56 -0300

    Sintaxe e Indexação

--- Sintaxe e Indentacao/Program.cs
+++ Sintaxe e Indentacao/Program.cs
@@ -4,3 +4,6 @@ Pessoa pessoa1 = new Pessoa();
 pessoa1.Nome = "Adailson";
 pessoa1.Idade = 31;
 pessoa1.Apresentar();
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
