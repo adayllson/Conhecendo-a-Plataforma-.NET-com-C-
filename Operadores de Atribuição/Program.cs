7ed131706c4909e142242a78afc56f75747e9588
Adailson <adayllson.almeida@gmail.com>
2022-09-22 17:21:21 -0300

    Operadores e Estruturas Condicionais

--- Operadores de Atribuição/Program.cs
+++ Operadores de Atribuição/Program.cs
@@ -6,3 +6,31 @@ int c = a + b;
 //c = c + 5;
 c += 5;
 Console.WriteLine(c);
+
+string x = "15-";
+int y = 0;
+int.TryParse(x, out y);
+
+Console.WriteLine(y);
+Console.WriteLine("Conversão realizada com sucesso!");
+
+int quantidadeEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida!");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Quantidade realizada!");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
