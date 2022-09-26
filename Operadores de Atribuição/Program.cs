434223bc3dd78febdc65ae9556e4b71bd1109fe4
Adailson <adayllson.almeida@gmail.com>
2022-09-26 17:52:48 -0300

    Calculadora Início

--- Operadores de Atribuição/Program.cs
+++ Operadores de Atribuição/Program.cs
@@ -1,36 +1,55 @@
-﻿
-int a = 10;
-int b = 20;
+﻿using Operadores_de_Atribuição.Models;
+// int a = 10;
+// int b = 20;
 
-int c = a + b;
-//c = c + 5;
-c += 5;
-Console.WriteLine(c);
+// int c = a + b;
+// //c = c + 5;
+// c += 5;
+// Console.WriteLine(c);
 
-string x = "15-";
-int y = 0;
-int.TryParse(x, out y);
+// string x = "15-";
+// int y = 0;
+// int.TryParse(x, out y);
 
-Console.WriteLine(y);
-Console.WriteLine("Conversão realizada com sucesso!");
+// Console.WriteLine(y);
+// Console.WriteLine("Conversão realizada com sucesso!");
 
-int quantidadeEstoque = 10;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+// int quantidadeEstoque = 10;
+// int quantidadeCompra = 4;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
-Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+// Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if (quantidadeCompra == 0)
-{
-    Console.WriteLine("Venda inválida!");
-}
-else if (possivelVenda)
-{
-    Console.WriteLine("Quantidade realizada!");
-}
-else
-{
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
-}
+// if (quantidadeCompra == 0)
+// {
+//     Console.WriteLine("Venda inválida!");
+// }
+// else if (possivelVenda)
+// {
+//     Console.WriteLine("Quantidade realizada!");
+// }
+// else
+// {
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+// }
+
+// Console.WriteLine("Digite uma letra: ");
+// string letra = Console.ReadLine();
+
+// switch (letra)
+// {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         Console.WriteLine($"A letra {letra} é uma vogal");
+//         break;
+//     default:
+//         Console.WriteLine($"A letra {letra} não é uma vogal");
+//         break;
+// }
+
+Calculadora calc = new Calculadora();
