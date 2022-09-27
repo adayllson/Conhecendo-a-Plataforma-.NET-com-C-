d2bb183c3a42ddaf0377f5c2351f334968d8ff8a
Adailson <adayllson.almeida@gmail.com>
2022-09-27 17:23:16 -0300

    Estrutura de Repetição - Início

--- Operadores de Atribuição/Program.cs
+++ Operadores de Atribuição/Program.cs
@@ -63,3 +63,13 @@ calc.Seno(30);
 calc.Conseno(30);
 calc.Tangente(30);
 calc.RaizQuadrada(144);
+
+// Estrutura de Repetição
+// Tabuada
+
+int numero = 5;
+
+for (int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
