1de09e186a5535982c074b579e1b644ee6cf3938
Adailson <adayllson.almeida@gmail.com>
2022-09-28 16:37:12 -0300

    Renomeando diretórios

--- 2º Parte/Program.cs
+++ 2º Parte/Program.cs
@@ -1,101 +1,101 @@
 ﻿using Operadores_de_Atribuição.Models;
-// int a = 10;
-// int b = 20;
+int a = 10;
+int b = 20;
 
-// int c = a + b;
-// //c = c + 5;
-// c += 5;
-// Console.WriteLine(c);
+int c = a + b;
+//c = c + 5;
+c += 5;
+Console.WriteLine(c);
 
-// string x = "15-";
-// int y = 0;
-// int.TryParse(x, out y);
+string x = "15-";
+int y = 0;
+int.TryParse(x, out y);
 
-// Console.WriteLine(y);
-// Console.WriteLine("Conversão realizada com sucesso!");
+Console.WriteLine(y);
+Console.WriteLine("Conversão realizada com sucesso!");
 
-// int quantidadeEstoque = 10;
-// int quantidadeCompra = 4;
-// bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
+int quantidadeEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEstoque >= quantidadeCompra;
 
-// Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
-// Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");
-// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+Console.WriteLine($"Quantidade em estoque: {quantidadeEstoque}");
+Console.WriteLine($"Quantidade desejada: {quantidadeCompra}");
+Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-// if (quantidadeCompra == 0)
-// {
-//     Console.WriteLine("Venda inválida!");
-// }
-// else if (possivelVenda)
-// {
-//     Console.WriteLine("Quantidade realizada!");
-// }
-// else
-// {
-//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
-// }
+if (quantidadeCompra == 0)
+{
+    Console.WriteLine("Venda inválida!");
+}
+else if (possivelVenda)
+{
+    Console.WriteLine("Quantidade realizada!");
+}
+else
+{
+    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+}
 
-// Console.WriteLine("Digite uma letra: ");
-// string letra = Console.ReadLine()!;
+Console.WriteLine("Digite uma letra: ");
+string letra = Console.ReadLine()!;
 
-// switch (letra)
-// {
-//     case "a":
-//     case "e":
-//     case "i":
-//     case "o":
-//     case "u":
-//         Console.WriteLine($"A letra {letra} é uma vogal");
-//         break;
-//     default:
-//         Console.WriteLine($"A letra {letra} não é uma vogal");
-//         break;
-// }
+switch (letra)
+{
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine($"A letra {letra} é uma vogal");
+        break;
+    default:
+        Console.WriteLine($"A letra {letra} não é uma vogal");
+        break;
+}
 
-// Calculadora calc = new Calculadora();
+Calculadora calc = new Calculadora();
 
-// calc.Somar(10, 30);
-// calc.Subtrair(10, 50);
-// calc.Multiplicar(15, 45);
-// calc.Dividir(2, 2);
-// calc.Potencia(2, 10);
-// calc.Seno(30);
-// calc.Conseno(30);
-// calc.Tangente(30);
-// calc.RaizQuadrada(144);
+calc.Somar(10, 30);
+calc.Subtrair(10, 50);
+calc.Multiplicar(15, 45);
+calc.Dividir(2, 2);
+calc.Potencia(2, 10);
+calc.Seno(30);
+calc.Conseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(144);
 
-// // Estrutura de Repetição
-// // Tabuada
+// Estrutura de Repetição
+// Tabuada
 
-// int numero = 5;
+int numero = 5;
 
-// for (int contador = 0; contador <= 10; contador++)
-// {
-//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
-// }
+for (int contador = 0; contador <= 10; contador++)
+{
+    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+}
 
-// int Numero = 7, Contador = 0;
+int Numero = 7, Contador = 0;
 
-// while (Contador <= 10)
-// {
-//     Console.WriteLine($"{Numero} x {Contador} = {Numero * Contador++}");
+while (Contador <= 10)
+{
+    Console.WriteLine($"{Numero} x {Contador} = {Numero * Contador++}");
 
-//     if (Contador == 6)
-//     {
-//         break;
-//     }
-// }
+    if (Contador == 6)
+    {
+        break;
+    }
+}
 
-//  int soma = 0, numero1 = 0;
+ int soma = 0, numero1 = 0;
 
-//  do
-//  {
-//     Console.WriteLine("Digite um número ou 0 para parar:");
-//     numero1 = Convert.ToInt32( Console.ReadLine());
-//     soma += numero1;
-//  } while(numero1 != 0);
+ do
+ {
+    Console.WriteLine("Digite um número ou 0 para parar:");
+    numero1 = Convert.ToInt32( Console.ReadLine());
+    soma += numero1;
+ } while(numero1 != 0);
 
-//     Console.WriteLine($"Total da soma dos números digitados é: {soma}");
+    Console.WriteLine($"Total da soma dos números digitados é: {soma}");
 
 string opcao;
 
